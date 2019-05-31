b29c55945f9f7e724931996f70d14c829686bc0f
Dalton <daltonvarussa@gmail.com>
2019-05-31 21:37:53 +0200

    51 - VS Solution for MultipleProjects (linking)

---
+++ 051-MakingAndWorkingWithLibraries/Game/Engine/Engine.cpp
@@ -0,0 +1,10 @@
+#include "Engine.h"
+#include <iostream>
+
+namespace engine
+{
+	void Print()
+	{
+		std::cout << "Engine is running!" << std::endl;
+	}
+}
---
+++ 051-MakingAndWorkingWithLibraries/Game/Game/Game.cpp
@@ -0,0 +1,11 @@
+#include "Engine.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Game Starting" << std::endl;
+	engine::Print();
+	std::getchar();
+
+	return 0;
+}
